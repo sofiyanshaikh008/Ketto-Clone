ed5c547a8368156dada2d9e80ef38682da773423
arun24hrs <2017arun.rana@gmail.com>
2022-09-30 02:13:27 +0530

    added divs of carousel

---
+++ scripts/index.js
@@ -0,0 +1,164 @@
+let trendingData = [
+    {
+        title: "My Little Boy’s Cancer Has Relapsed Twice But I’m Helpless. Please Save Him",
+        image: "https://kettocdn.gumlet.io/media/campaign/212000/212655/image/1f1a33100b0ce11487774ca00c4341f7d0364e69.jpg?w=480&dpr=1.3",
+        by: "by Family Friends",
+        amount: "₹ 4,21,29,615",
+        target: "₹ 5,00,00,000",
+        status: "Last donation an hour ago",
+        dayleft: "21",
+        support: "22041",
+        avatar: "FF",
+        a: 42129615,
+        t: 50000000
+    },
+    {
+        title: "Help 7yrs Old Paarnikka Beat Cancer",
+        image: "https://kettocdn.gumlet.io/media/campaign/372000/372126/image/01ce2a07f56735f26b62aac7c317696e57f1c3f9.png?w=480&dpr=1.3",
+        by: "by Rm Agarwal",
+        amount: "₹ 1,14,64,591",
+        target: "₹ 3,00,00,000",
+        status: "Last donation less than an hour ago",
+        dayleft: "34",
+        support: "6055",
+        avatar: "RA",
+        a: 11464591,
+        t: 30000000
+    },
+    {
+        title: "Offer A Helping Hand To Support Rubina Rodgers' Treatment",
+        image: "https://kettocdn.gumlet.io/media/campaign/281000/281759/image/60211afcc8a19.png?w=480&dpr=1.3",
+        by: "by Sabapathy J",
+        amount: "₹ 77,75,820",
+        target: "₹ 95,00,000",
+        status: "Last donation 3 hour ago",
+        dayleft: "14",
+        support: "2778",
+        avatar: "SJ",
+        a: 7775820,
+        t: 9500000
+    },
+    {
+        title: "My Young 30 Year Old Husband Is In Urgent Need Of Heart Transplant...",
+        image: "https://kettocdn.gumlet.io/media/campaign/686000/686992/image/6315de97732b3.jpg?w=768&dpr=1.5",
+        by: "by Sabapathy J",
+        amount: "₹ 7,75,820",
+        target: "₹ 30,00,000",
+        status: "Last donation 3 hour ago",
+        dayleft: "20",
+        support: "2778",
+        avatar: "SJ",
+        a: 775820,
+        t: 3000000
+    },
+    {
+        title: "My Nephew Is Suffering From Stroke. We Need Your Help To Provide For...",
+        image: "https://kettocdn.gumlet.io/media/campaign/691000/691314/image/631efcd202e88.jpg?w=768&dpr=1.5",
+        by: "by Munender Egam",
+        amount: "₹ 4,30,258",
+        target: "₹ 25,00,000",
+        status: "Last donation 3 hour ago",
+        dayleft: "20",
+        support: "277",
+        avatar: "ME",
+        a: 430258,
+        t: 2500000
+    },
+    {
+        title: "Help End Isolation For Children In Need: Empowering Holistically",
+        image: "https://kettocdn.gumlet.io/media/campaign/601000/601050/image/6236e4b1a747a.jpeg?w=768&dpr=1.5",
+        by: "by HEAL Foundation",
+        amount: "₹ 4,29,258",
+        target: "₹ 7,57,000",
+        status: "Last donation 3 hour ago",
+        dayleft: "39",
+        support: "169",
+        avatar: "HF",
+        a: 429258,
+        t: 757000
+    },
+    {
+        title: "Help Jungle Crows To Make Champions In Sport And Life!",
+        image: "https://kettocdn.gumlet.io/media/campaign/557000/557837/image/629f1a5fc7ce6.png?w=768&dpr=1.5",
+        by: "by Jungle Crows Foundation",
+        amount: "₹ 4,37,108",
+        target: "₹ 5,00,000",
+        status: "Last donation 5 hour ago",
+        dayleft: "28",
+        support: "69",
+        avatar: "JC",
+        a: 437108,
+        t: 500000
+    },
+    {
+        title: "Offer A Helping Hand To Support Khursid Alam Sardar's Treatment",
+        image: "https://kettocdn.gumlet.io/media/campaign/669000/669924/image/widb91a9adb6d470d1a44ba1ba2a2e994a6dc41de62.jpg?w=320&dpr=1.5",
+        by: "by Rajibuddin Pailan ",
+        amount: "₹ 3,21,055",
+        target: "₹ 8,00,000 ",
+        status: "Last donation 2 hour ago",
+        dayleft: "8 ",
+        support: "156",
+        avatar: "RP",
+        a: 321055,
+        t: 800000
+    },
+    {
+        title: "My Baby Battles For His Life And We Need Your Support To Save Him",
+        image: "https://kettocdn.gumlet.io/media/campaign/695000/695798/image/wid63286ab138222.jpg?w=320&dpr=1.5",
+        by: "by Rashmi Ranjan Mishra",
+        amount: "₹ 1,72,055",
+        target: "₹ 41,00,000 ",
+        status: "Last donation 2 hour ago",
+        dayleft: "36",
+        support: "146",
+        avatar: "RR",
+        a: 172055,
+        t: 4100000
+    }
+    ];
+
+    
+let appendTrending = () => {
+
+    let img = document.querySelectorAll(".tImg");
+    let title = document.querySelectorAll(".title");
+    let by = document.querySelectorAll(".name");
+    let raised = document.querySelectorAll(".raisedAmt");
+    let target = document.querySelectorAll(".target");
+    let leftTime = document.querySelectorAll(".timeLeft");
+    let supporters = document.querySelectorAll(".totalSupport");
+    
+    let achieveBar= document.querySelectorAll(".achieve")
+
+        for(let i=0; i<img.length; i++){
+            img[i].src = trendingData[i].image;
+
+            title[i].innerText = trendingData[i].title;
+
+            by[i].innerText = trendingData[i].by;
+
+            raised[i].innerHTML = `<p><span>${trendingData[i].amount}</span> raised out of</p>`;
+
+            target[i].innerText = trendingData[i].target;
+
+            leftTime[i].innerHTML = `<span>${trendingData[i].dayleft}</span>`;
+
+            supporters[i].innerText = `${trendingData[i].support}`;
+
+            let reach = trendingData[i].a*100/trendingData[i].t;
+            reach = reach.toFixed(0)
+            reach = +reach
+
+            achieveBar[i].style.width=`${reach}%`;
+
+            // console.log(reach)
+
+            // console.log(img[i]);
+        };
+
+    
+}
+
+appendTrending();
+
